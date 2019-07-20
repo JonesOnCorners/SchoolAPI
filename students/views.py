d80ce4c2a66ce494f422f3b6d1872b1db41e142c
JonesOnCorners <jayd.karale@gmail.com>
2019-07-20 21:15:51 +0530

    Added comments to the views

--- students/views.py
+++ students/views.py
@@ -9,22 +9,24 @@ from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnl
 
 # Create your views here.
 
-           
+ #View to handle student creation          
 class StudentCreate(generics.CreateAPIView):
     permission_classes  = [IsAuthenticated]
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
 
-
+#View that handles displaying entire student list
 class StudentList(generics.ListAPIView):
     permission_classes  = [IsAuthenticated]
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
-  
+
+#View that handles displaying details of a single student  
 class StudentDetailView(generics.RetrieveAPIView):    
     queryset = Student.objects.all()
     serializer_class = StudentSerializer        
 
+#View to handle put request for individual student
 class StudentEditView(generics.RetrieveUpdateDestroyAPIView):    
     queryset = Student.objects.all()
     serializer_class = StudentSerializer        
