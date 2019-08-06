963ddb4a912e2e0af0eb2cc68c7740e6b7b607fa
Karale <Jaydeep.Karale@newcorp.com>
2019-08-06 05:52:39 -0400

    Changed DB to MYSQL

--- School_Backend_Api/settings.py
+++ School_Backend_Api/settings.py
@@ -88,10 +88,18 @@ WSGI_APPLICATION = 'School_Backend_Api.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'UNIDB',
+        'USER': 'JAYDEEP.KARALE',
+        'PASSWORD': '123123ABC',
+        'HOST': '127.0.0.1', # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
+    },
 }
 
 
